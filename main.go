0a80d4bf6b77c569c23a72fc6a3893d0a13e2e41
難波 智貴 <1113053@PC1212.local>
2022-08-09 15:20:01 +0900

    feat: 修正

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("gtihub")
+	fmt.Println("gtihub")
+	fmt.Println("gtihub")
+	fmt.Println("gtihub")
+	fmt.Println("gtihub")
+	fmt.Println("gtihub")
+	fmt.Println("gtihub")
+	fmt.Println("gtihub")
 }
